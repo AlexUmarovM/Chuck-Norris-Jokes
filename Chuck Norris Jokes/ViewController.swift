568150547986738e98937c67f0a9af636fa2d45d
Alex Umarov <umarov.iosdev@gmail.com>
2020-06-19 00:03:22 +0300

    Initial Commit

---
+++ Chuck Norris Jokes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chuck Norris Jokes
+//
+//  Created by Александр Умаров on 19.06.2020.
+//  Copyright © 2020 Александр Умаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
